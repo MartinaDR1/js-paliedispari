1978f761386bfca363af0a2deea8fd77256dc748
MartinaDR1 <derosemartina933@gmail.com>
2023-03-16 18:20:21 +0100

    changes code

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ if(wordPal(userWord)){
 /*L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.*/
 
 // Chiedo all'utente di scegliere fra pari e dispari
-const choice = prompt('Scegli fra pari e dispari');
+let choice = prompt('Scegli fra pari e dispari');
 
 // Chiesto all'utente di inserire un numero
 const numberUser = Number(prompt('Inserisci un numero da 1 a 5'));
@@ -45,25 +45,25 @@ console.log(`Il pc genera il numero:${randNumber}`);
 
 // Sommo i due numeri
 function sum (num1, num2){
-    const risultFinal = num1 + num2;
-    return risultFinal
+    const resultFinal = num1 + num2;
+    return resultFinal
 };
 
 const result = sum(numberUser,randNumber)
 console.log(`La somma dei due numeri è: ${result}`);
 
-// Verifico se la somma restituisce un numero pari o dispari
+// Verifico se la somma restituisce un numero pari o dispari e stampo chi vince
 function isEven (numb){
-    if (numb % 2 == 0){
+    if ( choice == 'pari' && numb % 2 == 0 || choice =='dispari' && numb % 2 != 0){
         return true
     } else {
         return false
     }
 };
-
-// Stabilisco chi vince e stampo
-if (isEven == choice){
-    console.log('Hai vinto');
+ 
+if (isEven(result)){
+    console.log(`Complimenti! Hai vinto`);
 } else {
-    console.log('Ha vinto il pc');
+    console.log('Mi dispiace! Ha vinto il pc');
 }
+
