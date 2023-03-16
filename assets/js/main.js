77202079596e25b8d7489ac9ca583a750101c0f9
MartinaDR1 <derosemartina933@gmail.com>
2023-03-16 15:21:14 +0100

    add pali

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,23 @@
 /*Chiedere all’utente di inserire una parola 
 Creare una funzione per capire se la parola inserita è palindroma*/
 
+// Chiedo all'utente di inserire una parola
+const userWord = prompt('Inserisci una parola');
+console.log(userWord);
+
+// Creo una funzione 
+function wordPal (enterWord){
+    let word = enterWord.split('').reverse('').join('');
+    if (enterWord == word){
+        return true
+    } else {
+        return false
+    }
+}
+
+// Verifico se la parola inserita è palindroma o meno e stampo il risultato
+if(wordPal(userWord)){
+    console.log('Hai inserito una parola palindroma');
+} else {
+    console.log('Hai inserito una parola non palindroma');
+}
